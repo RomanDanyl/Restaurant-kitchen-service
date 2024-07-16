6a09a5ebd790430b708265a0a191d6064bcd1cca
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-07-16 12:01:28 +0300

    Add styles for home, login, logged_out pages

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -63,7 +64,7 @@ class DishTypeDetailView(generic.DetailView):
         return DishType.objects.prefetch_related("specialty_cooks", "dishes")
 
 
-class DishTypeCreateView(generic.CreateView):
+class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
     model = DishType
     fields = "__all__"
     success_url = reverse_lazy("kitchen:dish-type-list")
